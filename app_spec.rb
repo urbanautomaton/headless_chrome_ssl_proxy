64e1bf91407c400d6b127a8e2f56387609743d14
Simon Coffey <simon.coffey@futurelearn.com>
2018-03-16 16:13:34 +0000

    Eliminate capybara

--- app_spec.rb
+++ app_spec.rb
@@ -1,61 +1,33 @@
 require 'selenium/webdriver'
-require 'billy/capybara/rspec'
-require 'capybara/rspec'
+require 'billy'
 
-class App
-  def call(_)
-    [
-      '200',
-      { 'Content-Type' => 'text/html' },
-      [
-        <<~HTML
-          <html>
-          <body>
-          <iframe src="https://example.net/" id="iframe"></iframe>
-          </body>
-          </html>
-        HTML
-      ]
-    ]
+RSpec.describe 'Proxied HTTPS requests' do
+  let(:browser) do
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.headless! if ENV['HEADLESS']
+    options.add_argument('--user-data-dir=tmp/chrome')
+    options.add_argument('--enable-logging=stderr')
+    options.add_argument('--v=1')
+    options.add_argument("--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}")
+
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
+    capabilities[:acceptInsecureCerts] = true
+
+    Selenium::WebDriver.for(
+      :chrome,
+      desired_capabilities: capabilities,
+      options: options,
+      driver_opts: {
+        verbose: true,
+        log_path: 'chromedriver.log',
+        log_level: 3,
+      }
+    )
   end
-end
 
-Capybara.app = App.new
+  it 'makes a request via the proxy', type: :feature do
+    browser.get 'https://example.net/'
 
-Billy.configure do |c|
-  c.non_whitelisted_requests_disabled = true
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.headless! if ENV['HEADLESS']
-  options.add_argument("--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}")
-
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
-  capabilities[:acceptInsecureCerts] = true
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities,
-    options: options
-  )
-end
-
-RSpec.configure do |config|
-  config.before(:each, type: :feature) do
-    Capybara.current_driver = :headless_chrome
-  end
-end
-
-RSpec.describe App do
-  it 'does stuff', type: :feature do
-    proxy.stub('https://example.net:443/').and_return(:text => 'Foobar')
-
-    visit '/'
-
-    within_frame("iframe") do
-      expect(page).to have_content('Foobar')
-    end
+    expect(browser.page_source).to include('Example Domain')
   end
 end
