6a6d449bede5e7b996e7af1ead8748f37e5ae02f
Simon Coffey <simon.coffey@futurelearn.com>
2018-03-20 14:17:13 +0000

    Switch to remote selenium server

--- repro.rb
+++ repro.rb
@@ -6,26 +6,22 @@ Billy.configure do |config|
   config.logger = Logger.new('log/billy.log')
 end
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.headless! if ENV['HEADLESS']
-options.add_argument("--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}")
+chrome_args = [
+  "--disable-web-security",
+  "--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}"
+]
+chrome_args.push("--headless") if ENV["HEADLESS"]
 
-capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
-capabilities[:acceptInsecureCerts] = true
-
-browser = Selenium::WebDriver.for(
-  :chrome,
-  desired_capabilities: capabilities,
-  options: options,
-  driver_opts: {
-    verbose: true,
-    log_path: 'log/chromedriver.log'
-  }
+capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+  "chromeOptions" => { "args" => chrome_args },
+  "acceptInsecureCerts" => true
 )
 
-browser.get 'https://example.net/'
+driver = Selenium::WebDriver.for(:remote, desired_capabilities: capabilities)
 
-if browser.page_source.include?('Example Domain')
+driver.navigate.to 'https://example.net/'
+
+if driver.page_source.include?('Example Domain')
   puts 'Pass!'
 else
   puts 'Fail!'
